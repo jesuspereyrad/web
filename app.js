2b74caca49ffb90bd49cc87a0c60d0887349b0d3
Jesus <jesuspereyrad@gmail.com>
2017-06-12 08:49:01 -0400

    adding ass2

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
-const express = require('express');
-const app = express();
-const exphbs = require('express-handlebars');
-const helpers = require('./lib/helpers');
+var express = require("express");
+var app = express();
+var exphbs = require("express-handlebars");
+var helpers = require("./lib/helpers");
 
-app.use('/static', express.static(__dirname + '/public'))
+app.use("/static", express.static(__dirname + "/public"));
 
-app.engine('.hb', exphbs ({
-  defaultLayout: 'main',
-  extname: 'hb',
-  layoutsDir: 'views/layouts/',
-  partialsDir: 'views/partials/',
-  helpers: helpers 
+app.engine(".hb", exphbs({
+  defaultLayout: "main",
+  extname: "hb",
+  layoutsDir: "views/layouts/",
+  partialsDir: "views/partials/",
+  helpers: helpers
   }
 ));
 
-app.set('view engine', '.hb');
+app.set("view engine", ".hb");
 
-app.get('/', function (req, res) {
-  res.render('home');
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get('/about', function (req, res) {
-  res.render('about');
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
-// app.get('/about', function (req, res) {
-//   res.sendFile('about.html', options, function (err) {
-//     if (err) {
-//       console.log(err);
-//     }
-//   });
-// });
 
 app.listen(5331, function () {
   console.log("Server listen on port 5331");
